967ccb5d844ba705d6e8cd2650e28e76e23dee7a
FrancescoDui <francesco.d.off.ph@gmail.com>
2023-03-23 13:19:32 +0100

    aggiornamento esercitazione

--- esjava/src/esercitazione/lezione6/Triangolo.java
+++ esjava/src/esercitazione/lezione6/Triangolo.java
@@ -1,9 +1,7 @@
 package esercitazione.lezione6;
-
 import esercitazione.lezione6.interfaces.MisurePoligono;
-
-public abstract class  Triangolo implements MisurePoligono {
-    private static final Integer nlati=3;
+public abstract class  Triangolo implements MisurePoligono{
+    private static final Integer nlati = 3;
     @Override
     public abstract double calcolaArea();
     @Override
